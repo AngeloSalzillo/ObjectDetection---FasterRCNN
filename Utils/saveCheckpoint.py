c4cd5500089667f7e2def5a950639facf72dd061
AngeloSalzillo <a.salzillo.pro@gmail.com>
2026-01-13 16:42:36 +0100

    Create saveCheckpoint.py

---
+++ Utils/saveCheckpoint.py
@@ -0,0 +1,14 @@
+import torch
+
+
+def save_checkpoint(path, model, optimizer, epoch, metrics=None):
+    checkpoint = {
+        "epoch": epoch,
+        "model_state_dict": model.state_dict(),
+        "optimizer_state_dict": optimizer.state_dict(),
+        "metrics": metrics,
+    }
+    torch.save(checkpoint, path)
+
+
+
